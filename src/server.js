4e8ecc9f10ca79526335490c0ac518de80eed1c2
Kentska <kent.skarnaes@gmail.com>
2025-05-16 14:24:33 +0200

    uppd. server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+
+const channelRoutes = require('./routes/channelRoutes');
 const messageRoutes = require('./routes/messageRoutes');
+const subscriptionRoutes = require('./routes/subscriptionRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+app.use('/api/channels', channelRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/subscriptions', subscriptionRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(3000, () => console.log('Server running'));
